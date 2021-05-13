01de4c4d09ae4c3eeff9fc71ea5cc5da7f451d2e
anderleand <andersonleandro.desouza@gmail.com>
2021-05-13 18:10:27 -0300

    Update PessoaController.js

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,7 +1,7 @@
 const database = require('../models'); //Não há a necessidade de colocar o 'index.js' pq por padrão o JavaScript já procura um 'index.js' em models.
 
 class PessoaController {
-    
+
     static async pegaTodasAsPessoas(req, res) {
         try {
             const todasAsPessoas = await database.Pessoas.findAll();
@@ -72,7 +72,7 @@ class PessoaController {
                 }
             });
 
-            return res.status(200).json({mensagem: `id${id} deletado.`});
+            return res.status(200).json({mensagem: `id ${id} deletado.`});
         } catch (error) {
 
             return res.status(500).json(error.message);
